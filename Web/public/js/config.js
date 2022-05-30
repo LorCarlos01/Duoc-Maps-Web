968c3fda7ca64273c0caf60a36f908522049e398
LorCarlos <motarin_01@hotmail.com>
2022-05-29 23:06:47 -0400

    Se agrega al hosting de firebase con las bases de datos de las salas

--- Web/public/js/config.js
+++ Web/public/js/config.js
@@ -2,6 +2,7 @@
 firebase.initializeApp({
     apiKey: "AIzaSyBOSBQM-c-PklFYtAjIvcFwBF0waERHyB4",
     authDomain: "duocucmaps-b8772.firebaseapp.com",
+    databaseURL: "https://duocucmaps-b8772-default-rtdb.firebaseio.com",
     projectId: "duocucmaps-b8772",
     storageBucket: "duocucmaps-b8772.appspot.com",
     messagingSenderId: "442202982126",
