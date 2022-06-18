caeff1bc0677c9ed4d6253d9b0963695c1616356
LorCarlos <motarin_01@hotmail.com>
2022-06-18 15:53:18 -0400

    Cambios
    Se le agregan variados cambiar a la página web

--- Web/public/js/ver.js
+++ Web/public/js/ver.js
@@ -1,16 +1,28 @@
-const taskContainer = document.getElementById('sala');
-const taskContainer1 = document.getElementById('salas_p2');
-const taskContainer2 = document.getElementById('salas_p3');
-const taskContainer3 = document.getElementById('salas_p4');
-const taskContainer4 = document.getElementById('salas_p5');
-const taskContainer5 = document.getElementById('salas_p6');
+const taskContainer = document.getElementById('pisouno');
+const taskContainer1 = document.getElementById('pisodos');
+const taskContainer2 = document.getElementById('pisotres');
+const taskContainer3 = document.getElementById('pisocuatro');
+const taskContainer4 = document.getElementById('pisocinco');
+const taskContainer5 = document.getElementById('pisoseis');
+const taskContainer6 = document.getElementById('pisomenosdos');
+const taskContainer7 = document.getElementById('pisomenosuno');
+const taskContainer8 = document.getElementById('pdr');
+const taskContainer9 = document.getElementById('servicio');
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
 
     //Piso 1
-    db.collection("sala").get().then((querySnapshot) => {
+    db.collection("pisouno").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data().nombre_sala}`);
             taskContainer.innerHTML += 
-            `<div class="btn card card-body mt-1">
-            <h1>${doc.data().nombre_sala}</h1>
+            `<div class="btn card card-body mt-3">
+            <h3>Sala: ${doc.data().nombre}</h3>
+            <h5>Tipo: ${doc.data().tipo}</h5>
+            <h5>Escuela: ${doc.data().escuela}</h5>
+            <h5>Ubicación: ${doc.data().ubicacion}</h5>
+            <h5>Descripción: ${doc.data().descripcion}</h5>
             </div>`
             
             
@@ -19,11 +31,15 @@ const taskContainer5 = document.getElementById('salas_p6');
 
 
     //Piso 2
-    db.collection("salas_p2").get().then((querySnapshot) => {
+    db.collection("pisodos").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             taskContainer1.innerHTML += 
-            `<div class="btn  card card-body mt-1">
-            <h1>${doc.data().nombre_sala}</h1>
+            `<div class="card card-body mt-3">
+            <h3>Sala: ${doc.data().nombre}</h3>
+            <h5>Tipo: ${doc.data().tipo}</h5>
+            <h5>Escuela: ${doc.data().escuela}</h5>
+            <h5>Ubicación: ${doc.data().ubicacion}</h5>
+            <h5>Descripción: ${doc.data().descripcion}</h5>
             </div>`
             
             
@@ -31,11 +47,15 @@ const taskContainer5 = document.getElementById('salas_p6');
     });
 
     //piso 3
-    db.collection("salas_p3").get().then((querySnapshot) => {
+    db.collection("pisotres").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             taskContainer2.innerHTML += 
-            `<div class="btn  card card-body mt-1">
-            <h1>${doc.data().nombre_sala}</h1>
+            `<div class="btn  card card-body mt-3">
+            <h3>Sala: ${doc.data().nombre}</h3>
+            <h5>Tipo: ${doc.data().tipo}</h5>
+            <h5>Escuela: ${doc.data().escuela}</h5>
+            <h5>Ubicación: ${doc.data().ubicacion}</h5>
+            <h5>Descripción: ${doc.data().descripcion}</h5>
             </div>`
             
             
@@ -43,11 +63,15 @@ const taskContainer5 = document.getElementById('salas_p6');
     });
 
     //Piso 4
-    db.collection("salas_p4").get().then((querySnapshot) => {
+    db.collection("pisocuatro").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             taskContainer3.innerHTML += 
-            `<div class="btn  card card-body mt-1">
-            <h1>${doc.data().nombre_sala}</h1>
+            `<div class="btn  card card-body mt-3">
+            <h3>Sala: ${doc.data().nombre}</h3>
+            <h5>Tipo: ${doc.data().tipo}</h5>
+            <h5>Escuela: ${doc.data().escuela}</h5>
+            <h5>Ubicación: ${doc.data().ubicacion}</h5>
+            <h5>Descripción: ${doc.data().descripcion}</h5>
             </div>`
             
             
@@ -55,11 +79,15 @@ const taskContainer5 = document.getElementById('salas_p6');
     });
 
     //Piso 5
-    db.collection("salas_p5").get().then((querySnapshot) => {
+    db.collection("pisocinco").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             taskContainer4.innerHTML += 
-            `<div class="btn  card card-body mt-1">
-            <h1>${doc.data().nombre_sala}</h1>
+            `<div class="btn  card card-body mt-3">
+            <h3>Sala: ${doc.data().nombre}</h3>
+            <h5>Tipo: ${doc.data().tipo}</h5>
+            <h5>Escuela: ${doc.data().escuela}</h5>
+            <h5>Ubicación: ${doc.data().ubicacion}</h5>
+            <h5>Descripción: ${doc.data().descripcion}</h5>
             </div>`
             
             
@@ -67,13 +95,73 @@ const taskContainer5 = document.getElementById('salas_p6');
     });
 
     //Piso 6
-    db.collection("salas_p6").get().then((querySnapshot) => {
+    db.collection("pisoseis").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             taskContainer5.innerHTML += 
-            `<div class="btn  card card-body mt-1">
-            <h1>${doc.data().nombre_sala}</h1>
+            `<div class="btn  card card-body mt-3">
+            <h3>Sala: ${doc.data().nombre}</h3>
+            <h5>Tipo: ${doc.data().tipo}</h5>
+            <h5>Escuela: ${doc.data().escuela}</h5>
+            <h5>Ubicación: ${doc.data().ubicacion}</h5>
+            <h5>Descripción: ${doc.data().descripcion}</h5>
             </div>`
             
             
         });
     });
+
+    //Piso -2
+    db.collection("pisomenosdos").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            taskContainer6.innerHTML += 
+            `<div class="btn  card card-body mt-3">
+            <h3>Sala: ${doc.data().nombre}</h3>
+            <h5>Tipo: ${doc.data().tipo}</h5>
+            <h5>Escuela: ${doc.data().escuela}</h5>
+            <h5>Ubicación: ${doc.data().ubicacion}</h5>
+            <h5>Descripción: ${doc.data().descripcion}</h5>
+            </div>`        
+            });
+        });
+
+
+    //Piso -1
+    db.collection("pisomenosuno").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+        taskContainer7.innerHTML += 
+        `<div class="btn  card card-body mt-3">
+        <h3>Sala: ${doc.data().nombre}</h3>
+        <h5>Tipo: ${doc.data().tipo}</h5>
+        <h5>Escuela: ${doc.data().escuela}</h5>
+        <h5>Ubicación: ${doc.data().ubicacion}</h5>
+        <h5>Descripción: ${doc.data().descripcion}</h5>
+        </div>`
+                            
+            });
+        });
+
+        //Punto Reciclaje
+        db.collection("pdr").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+            taskContainer8.innerHTML += 
+            `<div class="btn  card card-body mt-3">
+            <h3>Nombre: ${doc.data().nombre}</h3>
+            <h5>Ubicacion: ${doc.data().ubicacion}</h5>
+            <h5>Descripcion: ${doc.data().descripcion}</h5>
+            </div>`
+                                
+                });
+            });
+
+            //Servicio
+            db.collection("servicio").get().then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                taskContainer9.innerHTML += 
+                `<div class="btn  card card-body mt-3">
+                <h3>Nombre: ${doc.data().nombre}</h3>
+                <h5>Ubicacion: ${doc.data().ubicacion}</h5>
+                <h5>Descripcion: ${doc.data().descripcion}</h5>
+                </div>`
+                                    
+                    });
+                });
